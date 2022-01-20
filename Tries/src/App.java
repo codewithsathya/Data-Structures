c97a2ef097f71c8df0c81c1a36c96847d85d9458
Sathiya Seelan <codewithsathya@gmail.com>
2022-01-20 10:12:05 +0530

    Insert method in trie with children in array

---
+++ Tries/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Trie trie = new Trie();
+        trie.insert("boy");
+        trie.insert("book");
+    }
+}
---
+++ Tries/src/Trie.java
@@ -0,0 +1,30 @@
+public class Trie {
+	private class Node{
+		public static int ALPHABET_SIZE = 26;
+		private char value;
+		private Node[] children = new Node[ALPHABET_SIZE];
+		private boolean isEndOfWord;
+
+		public Node(char value){
+			this.value = value;
+		}
+
+		@Override
+		public String toString(){
+			return "value=" + value;
+		}
+	}
+
+	Node root = new Node(' ');
+
+	public void insert(String word){
+		Node current = root;
+		for(char ch : word.toCharArray()){
+			int index = ch - 'a';
+			if(current.children[index] == null)
+				current.children[index] = new Node(ch);
+			current = current.children[index];
+		}
+		current.isEndOfWord = true;
+	}
+}
