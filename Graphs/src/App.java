2e031557fd73c0d16fd525c3c42a595a8f2dd7fd
Sathiya Seelan <codewithsathya@gmail.com>
2022-02-06 00:29:54 +0530

    Graph

--- Graphs/src/App.java
+++ Graphs/src/App.java
@@ -1,17 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Graph graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-        
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "C");
-        graph.addEdge("C", "D");
-        graph.addEdge("D", "A");
-        graph.addEdge("B", "D");
-        graph.print();
-        graph.traverse("A");
+        graph.addNode("0");
+        graph.addNode("1");
+        graph.addNode("2");
+        graph.addNode("3");
+        graph.addNode("4");
+        graph.addEdge("0", "1");
+        graph.addEdge("1", "2");
+        graph.addEdge("2", "3");
+        graph.addEdge("3", "4");
+        graph.addEdge("4", "0");
+        System.out.println(graph.hasCycle());
     }
 }
--- Graphs/src/Graph.java
+++ Graphs/src/Graph.java
@@ -77,7 +77,30 @@ public class Graph {
 		}
 	}
 
-	public void traverse(String root){
+	public List<String> topologicalSort(){
+		Set<Node> visited = new HashSet<>();
+		Stack<Node> stack = new Stack<>();
+		for(Node node: nodes.values()){
+			topologicalSort(node, visited, stack);
+		}
+
+		List<String> list = new ArrayList<>();
+		while(!stack.isEmpty()){
+			list.add(stack.pop().label);
+		}
+		return list;
+	}
+
+	private void topologicalSort(Node node, Set<Node> visited, Stack<Node> stack){
+		if(visited.contains(node)) return;
+		visited.add(node);
+		for(Node neighbour: adjacencyList.get(node)){
+				topologicalSort(neighbour, visited, stack);
+		}
+		stack.push(node);
+	}
+
+		public void traverseDepthFirstIterative(String root){
 		Node node = nodes.get(root);
 		if(node == null){
 			return;
@@ -95,5 +118,55 @@ public class Graph {
 				}
 			}
 		}
+	}
+
+	public void traverseBreadthFirst(String root){
+		Node node = nodes.get(root);
+		if(node == null){
+			return;
+		}
+		Queue<Node> queue = new ArrayDeque<>();
+		Set<Node> visited = new HashSet<>();
+		queue.add(node);
+		while(!queue.isEmpty()){
+			Node current = queue.remove();
+			if(!visited.contains(current)){
+				System.out.println(current.label);
+				visited.add(current);
+				for(Node neighbour : adjacencyList.get(current)){
+					queue.add(neighbour);
+				}
+			}
+		}
+	}
+
+	public boolean hasCycle(){
+		Set<Node> all = new HashSet<>();
+		Set<Node> visiting = new HashSet<>();
+		Set<Node> visited = new HashSet<>();
+		all.addAll(nodes.values());
+		while(!all.isEmpty()){
+			Node node = all.iterator().next();
+			if(hasCycle(node, all, visiting, visited))
+				return true;
+		}
+		return false;
+	}
+
+	private boolean hasCycle(Node node, Set<Node> all, Set<Node> visiting, Set<Node> visited){
+		all.remove(node);
+		visiting.add(node);
+		for(Node neighbour: adjacencyList.get(node)){
+			if(visited.contains(neighbour))
+				continue;
+			if(visiting.contains(neighbour))
+				return true;
+			if(hasCycle(neighbour, all, visiting, visited)){
+				return true;
+			}
+		}
+		visited.add(node);
+		visiting.remove(node);
+		return false;
 	}
 }
